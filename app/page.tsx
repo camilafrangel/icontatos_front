b162910a19f2803333f107775fe81e4398a8c125
Camila Rangel <camilafariasrangel@gmail.com>
2024-02-17 05:33:49 -0300

    fix: remove comment on contacts to test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import Contacts from "../components/contacts/contacts";
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* <Contacts/> */}
-      <h1>Testing deploy</h1>
+      <Contacts/>
     </main>
   );
 }

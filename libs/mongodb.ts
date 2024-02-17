26b9213bf3ea9b1736377838455dc8c73bd72b10
Camila Rangel <camilafariasrangel@gmail.com>
2024-02-17 01:20:26 -0300

    fix: fix environment issue

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -1,14 +1,16 @@
 import mongoose from "mongoose"
 
 const connectMongoDB = async () => {
-    try {
-        //Lembrar de mudar
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to MongoDB")
-
-    } catch (error) {
-        console.log(error)
-    }
+    if (process.env.MONGODB_URI) {
+        try {
+            await mongoose.connect(process.env.MONGODB_URI);
+            console.log("Connected to MongoDB");
+        } catch (error) {
+            console.error("Error connecting to MongoDB:", error);
+        }
+    } else {
+        console.error("MONGODB_URI environment variable is not set.");
+    }    
 }
 
 export default connectMongoDB;
